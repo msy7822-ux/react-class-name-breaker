50c9eabd7231f6b1ce2d74ee0d194b0e560e94ac
masahiro fukatsu <msy@masahironoMacBook-Pro.local>
2023-06-14 23:22:48 +0900

    work in progress(?)🚀

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,90 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import {
+  ExtensionContext,
+  workspace,
+  TextEditor,
+  window,
+  WorkspaceEdit,
+  Range,
+} from "vscode";
+import { getClassNames, getUtility } from "./utils/regex";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  console.log("activate");
+const decorate = (editor: TextEditor) => {
+  if (editor === null) {
+    return;
+  }
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "react-class-name-breaker" is now active!'
+  const document = editor.document;
+  const text = document.getText();
+  const classNames = getClassNames(text);
+  classNames?.map((className) => {
+    return className.value
+      .replace(/\n|,|'|`|"|{|}/g, " ")
+      .split(" ")
+      .filter(Boolean);
+  }) ?? [];
+
+  const regexes = [
+    /(?:\b(?:class(?:Name)?|tw)\s*=\s*(?:(?:{([^>}]+)})|(["'`][^"'`]+["'`])))/,
+    /(?:(clsx|classnames)\()([^)]+)\)/,
+  ];
+  const regex = new RegExp(regexes.map((r) => r.source).join("|"), "gm");
+
+  classNames.forEach((className) => {
+    const chars: any[] = [];
+    const utilities = getUtility(className.value, regex);
+    utilities.forEach((utility) => {
+      const start = document.positionAt(className.start + utility.start);
+      const end = document.positionAt(className.start + utility.end);
+      const range = new Range(start, end);
+      chars.push({ range });
+    });
+
+    console.log("chars", chars);
+    //     editor.setDecorations(decorator.decorator, chars);
+  });
+  // });
+};
+
+export async function activate(context: ExtensionContext) {
+  const editor = window.activeTextEditor;
+  const arr = decorate(editor!);
+  const doc = editor?.document;
+  const texts = doc?.getText();
+
+  // const classNames: string[] = [];
+
+  // classes.map((classNameBlock) => {
+  //   if (classNameBlock.length > 4) {
+  //     return classNameBlock.map((className, i) => {
+  //       const index = i + 1;
+
+  //       return className;
+  //     });
+  //   } else {
+  //     classNames.push(classNameBlock.join(" "));
+  //   }
+  // });
+
+  // console.log("resp", resp);
+
+  const newText = texts?.replace(/your_string/g, "\n") ?? "";
+
+  let fullRange = doc?.validateRange(
+    new Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE)
   );
+  let edit = new WorkspaceEdit();
+  edit.replace(doc?.uri!, fullRange!, newText);
+  const res = await workspace.applyEdit(edit);
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "react-class-name-breaker.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from react-class-name-breaker!"
-      );
-    }
-  );
+  // console.log("classes", classes);
 
-  context.subscriptions.push(disposable);
+  // workspace.onDidChangeTextDocument(
+  //   (_event) => {
+  //     console.log("onDidChangeTextDocument");
+  //   },
+  //   null,
+  //   context.subscriptions
+  // );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
